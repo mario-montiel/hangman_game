efec4ecb9110311852c0e7e09ac8d65d8f29a65e
mario-montiel <mario159_@hotmail.com>
2019-10-17 11:33:47 -0500

    pruebon

--- gato/juego.py
+++ gato/juego.py
@@ -2,13 +2,13 @@
 import numpy as np
 class Juego:
     # juego = Juego()
-    intentos = 1
-    jugador = 0
+    intentos = 0
+    jugador = ""
     turno = 0
     tablero = np.array([['*',' *', '*'], ['*', '*', '*'], ['*', '*', '*']])
-    position = list(set(['01', '02', '03', '11', '12', '13', '21', '22', '23']))
+    position = list(set(['00', '01', '02', '10', '11', '12', '20', '21', '22']))
     def posiciones(self):
-        while self.intentos > 0:
+        while self.intentos <= 0:
             self.turnos()
             # posicion = input("SELECCIONE LA POSICIÓN \n"
             # + "[* - * - *\n"
@@ -17,20 +17,60 @@ class Juego:
             posicion = input("SELECCIONA UNA POSICIÓN\n" + str(self.position) + "\n")
             if len(posicion) > 2 or len(posicion) < 2:
                 print("INGRESE LOS CARACTERES CORRECTOS PORFAVOR")
-                return False
-            print(posicion)
+                break
+            # print(posicion)
             print(str(posicion[0] + ',' + posicion[1]))
 
-            self.tablero[0,1] =  "x"
+            # print(self.tablero[2,1])
+
+            posicion1 = int(posicion[0])
+            posicion2 = int(posicion[1])
+
+            if self.jugador == "JUGADOR 1":
+                self.tablero[posicion1, posicion2] =  "x"
+            else:
+                self.tablero[posicion1, posicion2] =  "o"
+
+            print(self.tablero)
             self.turno += 1
+            self.ganar()
+        self.limpiar()
 
     def turnos(self):
         if self.turno % 2:
-            print("Jugador2")
+            self.jugador = "JUGADOR 2"
+            print(self.jugador)
         else:
-            print("Jugador1")
+            self.jugador = "JUGADOR 1"
+            print(self.jugador)
         # if turno == 0:
-    # def intentos(self):
+    def ganar(self):
+        if self.tablero[0,0] == 'x' and self.tablero[0,1] == 'x' and self.tablero[0,2] == 'x' or self.tablero[0,0] == 'o' and self.tablero[0,1] == 'o' and self.tablero[0,2] == 'o':
+            self.intentos = 1
+            print("GANASTE " + self.jugador)
+        elif self.tablero[1,0] == 'x' and self.tablero[1,1] == 'x' and self.tablero[1,2] == 'x' or self.tablero[1,0] == 'o' and self.tablero[1,1] == 'o' and self.tablero[1,2] == 'o':
+            self.intentos = 1
+            print("GANASTE")
+        elif self.tablero[2,0] == 'x' and self.tablero[2,1] == 'x' and self.tablero[2,2] == 'x' or self.tablero[2,0] == 'o' and self.tablero[2,1] == 'o' and self.tablero[2,2] == 'o':
+            self.intentos = 1
+            print("GANASTE")
+        elif self.tablero[0,0] == 'x' and self.tablero[1,1] == 'x' and self.tablero[2,2] == 'x' or self.tablero[0,0] == 'o' and self.tablero[1,1] == 'o' and self.tablero[2,2] == 'o':
+            self.intentos = 1
+            print("GANASTE")
+        elif self.tablero[2,0] == 'x' and self.tablero[1,1] == 'x' and self.tablero[0,2] == 'x' or self.tablero[2,0] == 'o' and self.tablero[1,1] == 'o' and self.tablero[0,2] == 'o':
+            self.intentos = 1
+            print("GANASTE")
+        elif self.tablero[0,0] == 'x' and self.tablero[1,0] == 'x' and self.tablero[2,0] == 'x' or self.tablero[0,0] == 'o' and self.tablero[1,0] == 'o' and self.tablero[2,0] == 'o':
+            self.intentos = 1
+            print("GANASTE")
+        elif self.tablero[0,1] == 'x' and self.tablero[1,1] == 'x' and self.tablero[2,1] == 'x' or self.tablero[0,1] == 'o' and self.tablero[1,1] == 'o' and self.tablero[2,1] == 'o':
+            self.intentos = 1
+            print("GANASTE")
+        elif self.tablero[0,2] == 'x' and self.tablero[1,2] == 'x' and self.tablero[2,2] == 'x' or self.tablero[0,2] == 'o' and self.tablero[1,2] == 'o' and self.tablero[2,2] == 'o':
+            self.intentos = 1
+            print("GANASTE")
 
-    # def dibujo (self):
-        # if posicion == '00'
+    def limpiar(self):
+        self.tablero = np.array([['*',' *', '*'], ['*', '*', '*'], ['*', '*', '*']])
+        self.intentos = 0
+        self.position = list(set(['01', '02', '03', '11', '12', '13', '21', '22', '23']))
